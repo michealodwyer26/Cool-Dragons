0768ca2b0c1e9b0354416677df40a7cb61f475f6
Micheal O'Dwyer <32364206+michealodwyer26@users.noreply.github.com>
2017-11-10 22:50:06 +0000

    Add files via upload

---
+++ music/smileyFace.py
@@ -0,0 +1,31 @@
+# MPT Senior - Week 1 - Programming Assignment 1
+# A program that prints a smiley funny face to the screen
+
+# Define the printFace function
+def printFace():
+	print("      ********      ")
+	print("    ************    ")
+	print("   ****      ****   ")
+	print("  ***          ***  ")
+	print(" ***            *** ")
+	print("***   ()    ()   ***")
+	print("**    ()    ()    **")
+	print("*     ()    ()     *")
+	print("*     () || ()     *")
+	print("*        ||        *")
+	print("*        ||        *")
+	print("*   **        **   *")
+	print("*   ***      ***   *")
+	print("**   ***    ***   **")
+	print("***   ********   ***")
+	print(" ***   ******   *** ")
+	print("  ***          ***  ")
+	print("   ****      ****  ")
+	print("    ************   ")
+	print("     **********    ")
+# end def
+
+# execute the function
+printFace()
+
+
